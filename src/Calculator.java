09f72af8870f8a1a33253f830b7395be4ef0c30f
Ripazaha <ripazaha.git@gmail.com>
2022-11-21 14:03:21 +0300

    Исправление деления на 0 в классе Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,8 +5,8 @@ public class Calculator {
 
     BinaryOperator<Integer> plus = (x, y) -> x + y; // для математических операций над двумя числами
     BinaryOperator<Integer> minus = (x, y) -> x - y;
-    BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> multiply =  (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> y > 0 ? (x / y) : y * -1;
 
     UnaryOperator<Integer> pow = x -> x * x; // для произведения математических операций над одним числом
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
@@ -14,4 +14,5 @@ public class Calculator {
     Predicate<Integer> isPositive = x -> x > 0; // для определения положительное ли число
 
     Consumer<Integer> println = System.out::println; // для вывода числа в консоль
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
-//        int b = calc.minus.apply(1,1); // Ошибка "/ by zero" деление на 0
-        int b = calc.minus.apply(4, 1);
+        int b = calc.minus.apply(1,1); // Ошибка "/ by zero" деление на 0
+        //int b = calc.minus.apply(4, 1);
         int c = calc.devide.apply(a, b);
 
         calc.println.accept(c);
